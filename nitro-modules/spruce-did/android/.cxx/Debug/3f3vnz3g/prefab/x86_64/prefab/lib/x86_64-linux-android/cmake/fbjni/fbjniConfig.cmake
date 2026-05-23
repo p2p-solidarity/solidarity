2f9affd514f449b52299f98ac361b926dbbf98ce
kidneyweakx <kidneyweakx@gmail.com>
2026-05-24 07:17:22 +0900

    chore(nitro-spruce-did): gitignore .cxx Android build artifacts

--- nitro-modules/spruce-did/android/.cxx/Debug/3f3vnz3g/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/kidney/.gradle/caches/9.3.1/transforms/6f1f7af63467440b9fe6f23d6ac7fd9b/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/kidney/.gradle/caches/9.3.1/transforms/6f1f7af63467440b9fe6f23d6ac7fd9b/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
